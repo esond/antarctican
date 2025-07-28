924353b69bf8cc24d79ce14c0651c73de91222c2
Eric Sondergard <4650644+esond@users.noreply.github.com>
2025-07-28 11:01:33 -0600

    Add script for cleaning empty directories from share disks

---
+++ user-scripts/clean-empty-share-directories.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Replace with the name of the share you want to clean.
+share_name="data"
+
+# Notify start
+/usr/local/emhttp/plugins/dynamix/scripts/notify \
+  -s "Clean empty directories" \
+  -d "Empty directory cleaner starting @ $(date +%H:%M:%S)."
+
+for disk_path in /mnt/disk[0-9]*; do
+  # Skip non-directories, if they were to exist for some reason.
+  [ -d "$disk_path" ] || continue
+
+  target_path="$disk_path/$share_name"
+
+  echo "Deleting empty directories from $target_path..."
+  
+  # `find` can only delete empty directories, so this should be safe.
+  [ -d "$target_path" ] && find "$target_path" -type d -empty -delete -print
+done
+
+# Notify end
+/usr/local/emhttp/plugins/dynamix/scripts/notify \
+  -s "Clean empty directories" \
+  -d "Empty directory cleaner finished @ $(date +%H:%M:%S)."
